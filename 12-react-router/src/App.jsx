3081e6c98d8e1973d38ecb5f58531de53c632c20
Swapnil Agarwal <agarwalswapnil222@gmail.com>
2025-07-03 22:56:11 +0530

    inclusion of header and footer layout in the routes

--- 12-react-router/src/App.jsx
+++ 12-react-router/src/App.jsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import { Contact } from './Pages/Contact';
-import { Movie } from './Pages/Movie';
-import { About } from './Pages/About';
-import { Home } from './Pages/Home';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Contact } from "./Pages/Contact";
+import { Movie } from "./Pages/Movie";
+import { About } from "./Pages/About";
+import { Home } from "./Pages/Home";
+import { Layout } from "./Pages/Layout";
 
 function App() {
   const Router = createBrowserRouter([
     {
-      path:"/",
-      element:<Home/>,
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/movie",
+          element: <Movie />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+      ],
     },
-    {
-      path:"/about",
-      element:<About/>,
-    },
-    {
-      path:"/movie", 
-      element:<Movie/>,
-    },
-    {
-      path:"/contact",
-      element:<Contact/>,
-    }
-  ])
+  ]);
 
-  return <RouterProvider router={Router}/>
+  return <RouterProvider router={Router} />;
 }
 
-export default App
+export default App;
